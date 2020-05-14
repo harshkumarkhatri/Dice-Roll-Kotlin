f04e7c7d51ef0719b4a6d93dc08feac16818033a
harshkumarkhatri <mailharshkhatri@gmail.com>
2020-05-14 20:09:18 +0530

    Solved the count up button problem

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,12 +13,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
+        val countButton:Button=findViewById(R.id.button)
+        countButton.setOnClickListener { countButton() }
     }
     private fun rollDice(){
 //        Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show()
         val resultText:TextView=findViewById(R.id.textView)
         val randInt=(1..6).random()
         resultText.text=randInt.toString()
-
+    }
+    private fun countButton(){
+        val resultText:TextView=findViewById(R.id.textView)
+        val buttonText:Button=findViewById(R.id.button)
+        if(resultText.text=="Chaliye Shuru karte hai!"){
+            buttonText.text= '1'.toString()
+        }else if(resultText.text=="6"){
+            buttonText.text='6'.toString()
+        }else{
+            var resultInt=resultText.text.toString().toInt()
+            resultInt++;
+            buttonText.text= resultInt.toString()
+        }
     }
 }
