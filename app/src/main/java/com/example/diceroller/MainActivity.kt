8c040bad1dfbc8fa284d77772a7e55d48520597a
harshkumarkhatri <mailharshkhatri@gmail.com>
2020-05-14 21:46:45 +0530

    Changed variable with images of dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,26 +14,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
-        val countButton:Button=findViewById(R.id.button)
-        countButton.setOnClickListener { countButton() }
+//        val countButton:Button=findViewById(R.id.button)
+//        countButton.setOnClickListener { countButton() }
     }
     private fun rollDice(){
 //        Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show()
-        val resultText:TextView=findViewById(R.id.textView)
+//        val resultText:TextView=findViewById(R.id.textView)
+        val diceImage:ImageView=findViewById(R.id.dice_image)
         val randInt=(1..6).random()
-        resultText.text=randInt.toString()
-    }
-    private fun countButton(){
-        val resultText:TextView=findViewById(R.id.textView)
-        val buttonText:Button=findViewById(R.id.button)
-        if(resultText.text=="Chaliye Shuru karte hai!"){
-            buttonText.text= '1'.toString()
-        }else if(resultText.text=="6"){
-            buttonText.text='6'.toString()
-        }else{
-            var resultInt=resultText.text.toString().toInt()
-            resultInt++;
-            buttonText.text= resultInt.toString()
+        val drawableResource = when (randInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        diceImage.setImageResource(drawableResource)
+//        val randInt=(1..6).random()
+//        resultText.text=randInt.toString()
     }
+//    private fun countButton(){
+//        val resultText:TextView=findViewById(R.id.textView)
+//        val buttonText:Button=findViewById(R.id.button)
+//        if(resultText.text=="Chaliye Shuru karte hai!"){
+//            buttonText.text= '1'.toString()
+//        }else if(resultText.text=="6"){
+//            buttonText.text='6'.toString()
+//        }else{
+//            var resultInt=resultText.text.toString().toInt()
+//            resultInt++;
+//            buttonText.text= resultInt.toString()
+//        }
+//    }
 }
